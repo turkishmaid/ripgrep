064e9ee30ee56f308ad0c78fc24f6e90a85880c7
Tomek <tolek@tuta.com>
2025-07-20 23:38:31 +0200

    ignore/types: add gleam
    [Gleam] is a general-purpose, concurrent, functional high-level
    programming language that compiles to Erlang or JavaScript source code.
    
    Closes #3105
    
    [Gleam]: https://gleam.run/

--- crates/ignore/src/default_types.rs
+++ crates/ignore/src/default_types.rs
@@ -90,6 +90,7 @@ pub(crate) const DEFAULT_TYPES: &[(&[&str], &[&str])] = &[
     (&["fsharp"], &["*.fs", "*.fsx", "*.fsi"]),
     (&["fut"], &["*.fut"]),
     (&["gap"], &["*.g", "*.gap", "*.gi", "*.gd", "*.tst"]),
+    (&["gleam"], &["*.gleam"]),
     (&["gn"], &["*.gn", "*.gni"]),
     (&["go"], &["*.go"]),
     (&["gprbuild"], &["*.gpr"]),
