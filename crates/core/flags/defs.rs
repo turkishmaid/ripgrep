1ef939442f2e1ed0650dd5e13c06fce455b569f7
Lucas Garron <code@garron.net>
2025-08-07 18:39:34 -0700

    doc: clarify that `.git` is covered by `--hidden` and not `--ignore-vcs`
    Fixes #3121, Closes #3122

--- crates/core/flags/defs.rs
+++ crates/core/flags/defs.rs
@@ -2752,6 +2752,11 @@ ripgrep.
 A file or directory is considered hidden if its base name starts with a dot
 character (\fB.\fP). On operating systems which support a "hidden" file
 attribute, like Windows, files with this attribute are also considered hidden.
+.sp
+Note that \flag{hidden} will include files and folders like \fB.git\fP
+regardless of \flag{no-ignore-vcs}. To exclude such paths when using
+\flag{hidden}, you must explicitly ignore them using another flag or ignore
+file.
 "#
     }
 
@@ -4603,11 +4608,15 @@ impl Flag for NoIgnoreVcs {
     }
     fn doc_long(&self) -> &'static str {
         r"
-When given, filter rules from source control ignore files (e.g., \fB.gitignore\fP)
-are not respected. By default, ripgrep respects \fBgit\fP's ignore rules for
-automatic filtering. In some cases, it may not be desirable to respect the
-source control's ignore rules and instead only respect rules in \fB.ignore\fP
-or \fB.rgignore\fP.
+When given, filter rules from source control ignore files (e.g.,
+\fB.gitignore\fP) are not respected. By default, ripgrep respects \fBgit\fP's
+ignore rules for automatic filtering. In some cases, it may not be desirable
+to respect the source control's ignore rules and instead only respect rules in
+\fB.ignore\fP or \fB.rgignore\fP.
+.sp
+Note that this flag does not directly affect the filtering of source control
+files or folders that start with a dot (\fB.\fP), like \fB.git\fP. These are
+affected by \flag{hidden} and its related flags instead.
 .sp
 This flag implies \flag{no-ignore-parent} for source control ignore files as
 well.
